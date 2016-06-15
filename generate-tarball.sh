1bdaafd79b1a01f56728f871e07b90b48c69171f
V1TSK <vitaly@easycoding.org>
2016-06-15 22:14:19 +0500

    Updated script.

--- generate-tarball.sh
+++ generate-tarball.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-TARBALL=purple-skypeweb
+NAME=purple-skypeweb
 
-git clone -q https://github.com/EionRobb/skype4pidgin.git
+git clone -q --depth=1 --branch=master https://github.com/EionRobb/skype4pidgin.git
 cd skype4pidgin
 
 COMMIT=$(git log -n 1 --format=%H)
 SHORTCOMMIT=${COMMIT:0:7}
 
-mv skypeweb ../$TARBALL-$COMMIT
+mv skypeweb ../$NAME-$COMMIT
 cd ..
 
-tar -cJf $TARBALL-$SHORTCOMMIT.tar.xz $TARBALL-$COMMIT
-rm -rf $TARBALL-$COMMIT skype4pidgin
+tar -cJf $NAME-$SHORTCOMMIT.tar.xz $NAME-$COMMIT
+rm -rf $NAME-$COMMIT skype4pidgin
 
 echo commit0 $COMMIT
-echo Source0 $TARBALL-$SHORTCOMMIT.tar.xz
+echo Source0 $NAME-$SHORTCOMMIT.tar.xz
